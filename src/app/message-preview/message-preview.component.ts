6bdf38b7c820914f578e2cf0702e6950df57b12e
JasonXBlue <JasonX.blue1@gmail.com>
2020-04-05 15:24:08 -0500

    finished

--- src/app/message-preview/message-preview.component.ts
+++ src/app/message-preview/message-preview.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit, Input } from "@angular/core";
-
+import message from "../messages";
 @Component({
   selector: "app-message-preview",
   templateUrl: "./message-preview.component.html",
   styleUrls: ["./message-preview.component.css"]
 })
 export class MessagePreviewComponent implements OnInit {
-  @Input() message = {};
+  //@Input() message: {};
+  @Input() message = {
+    id: 0,
+    name: "",
+    date: "",
+    message: ""
+  };
   constructor() {}
 
   ngOnInit() {}
